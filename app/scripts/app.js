b266602c7c853df7a9071264e85d917ef7cf64b7
Vadim Ivanov <sferamail33@yandex.ru>
2017-02-12 21:25:24 +0300

    Сделан slider

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,37 @@
 import noUiSlider from 'nouislider';
 
 const slider = document.querySelector('.slider');
+const sliderValues = ['Не владею', 'Использую готовые решения', 'Использую готовые решения и умею их переделывать',
+'Пишу сложный JS с нуля'];
+
+const applySliderValues = function (sliderNode, values) {
+	const valueNodes = [].slice.call(sliderNode.querySelectorAll('.noUi-value-large'));
+
+	valueNodes.forEach((valueNode, index) => {
+		const value = valueNode.innerHTML;
+		valueNode.innerHTML = values[index];
+
+		valueNode.addEventListener('click', () => {
+			sliderNode.noUiSlider.set(value);
+		});
+	});
+};
 
 noUiSlider.create(slider, {
-	start: 40,
-	connect: 'lower',
+	start: 0,
+	snap: true,
 	range: {
 		min: 0,
+		'19%': 19,
+		'49%': 50,
 		max: 100
+	},
+	pips: {
+		mode: 'values',
+		values: [0, 19, 50, 100],
+		density: 100,
+		stepped: true
 	}
 });
 
-console.log(slider);
+applySliderValues(slider, sliderValues);
